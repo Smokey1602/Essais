04c43b7a6d8ce11e86a05afe0f7ed4e98b114352
Jean-Camille Lebreton <j.c.lebreton@hotmail.fr>
2017-04-27 12:41:44 +0200

    Add files via upload

--- essaii2c.sh
+++ essaii2c.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-heure = i2cget -y 1 0x32 0x02 
+seconde	= 	i2cget -y 1 0x32 0x00
+minute	=	i2cget -y 1 0x32 0x01
+heure	= 	i2cget -y 1 0x32 0x02 
+jour	=	i2cget -y 1 0x32 0x04
+mois	=	i2cget -y 1 0x32 0x05
+annee	=	i2cget -y 1 0x32 0x06
+
+#on va tester chacun des points et convertir sa valeur exadécimale en valeur décimale
+if [$seconde & (1u <<0)]
+then
+		echo "+1sec"
+else
+		echo "+0sec"
+fi
+
+	
 echo $heure
